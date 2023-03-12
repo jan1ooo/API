ac4dd939b1c015985b09ad5e254915152771dd29
Jânio Filho <filhojanio67@gmail.com>
2023-03-12 20:30:01 -0300

    Front-end já funcionando, configurei o banco de dados

---
+++ front-end/script.js
@@ -0,0 +1,41 @@
+const formulario = document.querySelector("form");
+const inputNome =document.querySelector(".nome");
+const inputEmail = document.querySelector(".email");
+const inputSenha = document.querySelector(".senha");
+const inputTelefone = document.querySelector(".telefone");
+
+function cadastrar(){
+
+    fetch("http://localhost:8080/cadastrar",
+        {
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                nome: inputNome.value,
+                email: inputEmail.value,
+                senha: inputSenha.value, 
+                telefone: inputTelefone.value
+            })    
+        }
+    )
+    .then(function (res) {console.log(res)})
+    .then(function (res) {console.log(res)})
+};
+
+function limpar(){
+    inputNome.value = "",
+    inputEmail.value = "",
+    inputSenha.value = "", 
+    inputTelefone.value = ""
+};
+
+formulario.addEventListener('submit', function (event){
+    event.preventDefault();
+
+    cadastrar();
+    limpar();
+    
+})
