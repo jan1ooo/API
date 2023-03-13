d61cf05dedccd69172d8c906d0836c13079be4b3
Jânio Filho <filhojanio67@gmail.com>
2023-03-13 19:09:23 -0300

    Criação dos Endpoints

--- back-end/src/main/java/br/com/projetoapi/projeto/controller/UsuarioController.java
+++ back-end/src/main/java/br/com/projetoapi/projeto/controller/UsuarioController.java
@@ -3,19 +3,40 @@ package br.com.projetoapi.projeto.controller;
 import br.com.projetoapi.projeto.DAO.IUsuario;
 import br.com.projetoapi.projeto.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
     @Autowired
     private IUsuario dao;
 
-    @GetMapping("/usuarios")
+    @GetMapping
     public List<Usuario> listaUsuarios (){
         return (List<Usuario>) dao.findAll();
     }
+
+    @PostMapping
+    public Usuario criarUsurio (@RequestBody Usuario usuario){
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+
+    @PutMapping
+    public Usuario editarUsurio (@RequestBody Usuario usuario){
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+
+    @DeleteMapping("/{id}")
+    public Optional<Usuario> excluirUsuario(@PathVariable Integer id){
+        Optional<Usuario> usuario = dao.findById(id);
+        dao.deleteById(id);
+        return usuario;
+    }
+
 }
