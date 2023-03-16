897464c6f6df66d78aaf00cf1bbd117667d16e73
Jânio Filho <filhojanio67@gmail.com>
2023-03-16 20:46:08 -0300

    Inserção do lombok para dimiuir o código

--- back-end/src/main/java/br/com/projetoapi/projeto/model/Usuario.java
+++ back-end/src/main/java/br/com/projetoapi/projeto/model/Usuario.java
@@ -1,7 +1,11 @@
 package br.com.projetoapi.projeto.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "usuario")
 public class Usuario {
@@ -22,43 +26,5 @@ public class Usuario {
     @Column (name = "telefone", length= 15, nullable = true)
     private String telefone;
 
-    public Integer getId() {
-        return id;
-    }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
 }
